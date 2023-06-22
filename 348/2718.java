0e23feeafd42a34e4ca20d53018d15c39bdda694
halder_410 <sneha2020sonu@gmail.com>
2023-06-22 23:41:39 +0530

    2718

---
+++ 348/2718.java
@@ -0,0 +1,28 @@
+class Solution {
+    public long matrixSumQueries(int n, int[][] queries) {
+        Set<Integer> rowVisited = new HashSet<>();
+        Set<Integer> columnVisited = new HashSet<>();
+        int row = n;
+        int column = n;
+        long sum = 0;
+        for(int i=queries.length-1; i>=0; i--) {
+            int index = queries[i][1];
+            int value = queries[i][2];
+            if(queries[i][0]==0) {
+                if(!rowVisited.contains(index)) {
+                    sum += column * value;
+                    rowVisited.add(index);
+                    row--;
+                }
+            }
+            else {
+                if(!columnVisited.contains(index)) {
+                    sum += row * value;
+                    columnVisited.add(index);
+                    column--;
+                }
+            }
+        }
+        return sum;
+    }
+}
